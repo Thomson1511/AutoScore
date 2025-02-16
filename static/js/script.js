93bb6a8fb9f6763c3f441b99032da0222990556d
Thomson1511 <kovacstomi130@gmail.com>
2025-02-16 15:49:12 +0100

    vissza állítás

--- static/js/script.js
+++ static/js/script.js
@@ -1,22 +1,7 @@
-let selectedGame = null;
 let selectedPlayers = null;
-
-function startGame(game) {
-    selectedGame = game;
-    document.getElementById("players-selection").style.display = "block";  // Megjelenítjük a játékosok választási lehetőségét
-}
 
 function selectPlayers(players) {
     selectedPlayers = players;
     document.getElementById("start-game-container").style.display = "block";  // Megjelenítjük a Start Game gombot
-    document.getElementById("start-game-btn").innerText = `Start ${selectedGame} for ${selectedPlayers} Player(s)`;
-}
-
-function startActualGame() {
-    if (selectedGame && selectedPlayers) {
-        alert(`Starting ${selectedGame} for ${selectedPlayers} player(s)`);
-        // Itt átirányíthatunk egy másik oldalra vagy elkezdhetjük a játékot
-    } else {
-        alert("Please select a game and number of players.");
-    }
+    document.getElementById("start-game-btn").innerText = `Start ${selectedGame} for ${selectedPlayers} Player(s) with ${startingScore}`;
 }
